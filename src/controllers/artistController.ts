65c9d11ad2b19ebec7d4b87cdfdcca4b04ec8718
Ramez Taher <ramezgamer11@gmail.com>
2023-11-01 22:44:12 +0100

    feat: Fetch data from API

--- src/controllers/artistController.ts
+++ src/controllers/artistController.ts
@@ -1,15 +1,33 @@
+import axios from "axios"
 import { Response, Request } from "express"
 
 export default class ArtistController implements ArtistController {
   async getArtistsByName(req: Request, res: Response) {
     try {
-      res
-        .status(200)
-        .json({ success: true, statusCode: "200", message: "artist" })
+      const { name } = req.query
+      if (!name) {
+        return res.status(401).json({
+          success: false,
+          statusCode: 401,
+          message: "please enter a name",
+        })
+      }
+
+      let URL = `https://ws.audioscrobbler.com/2.0/?method=artist.search&artist=${name}&api_key=daddd748c6487fea986fbe85f513c45f&format=json`
+      const data: any = await axios.get(URL)
+
+      return res.status(200).json({
+        success: true,
+        statusCode: 200,
+        data: data.data?.results?.artistmatches,
+      })
     } catch (error) {
-      res
-        .status(500)
-        .json({ success: false, statusCode: "500", message: "error" })
+      console.log(error)
+      res.status(500).json({
+        success: false,
+        statusCode: 500,
+        message: "error",
+      })
     }
   }
 }
