19585472fce55616b72ec9a829eb05d0eda6da5c
Ramez Taher <ramezgamer11@gmail.com>
2023-11-05 16:34:15 +0100

    fix: remove setup and mocks from testing

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/__tests__"],
-  setupFiles: ["<rootDir>/__tests__/setup.ts"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!**/node_modules/**"],
+  testPathIgnorePatterns: ["<rootDir>/__tests__/mocks/"],
+  modulePathIgnorePatterns: ["<rootDir>/__tests__/setup.ts"],
 }
